52dbd45a224ffc5ca2bf38f7066163708d3a50a6
dcapwell <dcapwell@gmail.com>
2014-08-31 22:25:34 -0700

    added a nix setup script that installs packages

---
+++ nix/README.md
@@ -0,0 +1,3 @@
+# Nix
+
+This section is to try to setup my environment with nix package manager.  
---
+++ nix/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+if ! type nix-env > /dev/null ; then 
+  bash <(curl https://nixos.org/nix/install)
+  export PATH="~/.nix-profile/bin/:~/.nix-profile/sbin/:$PATH"
+fi
+
+# Ruby and Nix don't go hand in hand ATM
+# Don't use nix for Ruby packages
+packages=(
+  clojure-1.6.0
+  scala-2.11.0 
+  ghc-7.8.3
+  apache-maven-3.1.1
+  tree-1.7.0
+  curl-7.36.0
+  tmux-1.9a
+  wget-1.15
+  git-2.1.0
+)
+
+nix-env -i "${packages[@]}"
